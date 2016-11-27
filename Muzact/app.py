643516de034f1e8723b3f0e242f896676071f715
Vikash Kothary <kothary.vikash@gmail.com>
2016-11-27 06:12:36 +0000

    What I said last time

--- Muzact/app.py
+++ Muzact/app.py
@@ -1,12 +1,23 @@
 from flask import Flask
-#from app import views
+import requests, json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-	return 'Hello World'
+    return shazam_request()
 
-if __name__=='__main__':
-	app.run('0.0.0.0', debug='True')
+def shazam_request():
+    headers = {'X-Shazam-Api-Key': '03789B8E-A8CE-4229-A880-7FDE4C4FAEFC',
+	'Content-Type':'application/octet-stream'}
+    payload = open('assets/hack2.wav', 'rb').read()
+    url = 'http://beta.amp.shazam.com/partner/recognise'
+    
+    response = requests.post(url, data=payload, headers=headers)
+    res = json.loads(response)
+    artist = res['matches'][0]['metadata']['artist']
+    return artist 
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug = 'True')
